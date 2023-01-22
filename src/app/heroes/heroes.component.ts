f1f8683a2cbe6990a0b72fda0549c208c02e92fc
Siriwan <lukmai_mee@hotmail.com>
2023-01-22 22:20:05 +0700

    add service

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  // to use service, require to intected in constructure first
+  constructor(private heroService :HeroService, private messageService:MessageService){}
+
+  heroes: Hero[] = [];
+  selectedHero? : Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  // use subscribe to asyn fetch
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero:Hero):void{
+    this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+}
